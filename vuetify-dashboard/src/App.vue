0390b0524991cd947037f18e27dbd3220556f8c3
Elisha Benjamin <malishben360@gmail.com>
2022-10-29 17:33:53 +0100

    Header, login page and footer

---
+++ vuetify-dashboard/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+	<v-app>
+		<v-app-bar app dark color="primary">
+			<v-app-bar-title>Vuetify Dashboard</v-app-bar-title>
+			<v-spacer></v-spacer>
+			<v-btn text>Home</v-btn>
+			<v-btn text>Login</v-btn>
+		</v-app-bar>
+		<v-main>
+			<v-card width="400" class="mx-auto mt-5">
+				<v-card-title>
+					<h1 class="display-1">Login</h1>
+				</v-card-title>
+				<v-card-text>
+					<v-form>
+						<v-text-field
+							label="Username"
+							prepend-icon="mdi-account-circle"
+						></v-text-field>
+						<v-text-field
+							label="password"
+							:type="showPassword ? 'text' : 'password'"
+							:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+							prepend-icon="mdi-lock"
+							@click:append="showPassword = !showPassword"
+						></v-text-field>
+					</v-form>
+				</v-card-text>
+				<v-divider></v-divider>
+				<v-card-actions>
+					<v-btn color="success">Register</v-btn>
+					<v-spacer></v-spacer>
+					<v-btn color="info">Login</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-main>
+		<v-footer app color="primary" padless>
+			<v-row justify="center" no-gutters>
+				<v-btn
+					v-for="link in links"
+					:key="link"
+					color="primary"
+					text
+					class="my-2"
+				>
+					{{ link }}
+				</v-btn>
+				<v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+					{{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+				</v-col>
+			</v-row>
+		</v-footer>
+	</v-app>
+</template>
+
+<script>
+export default {
+	name: 'App',
+
+	components: {},
+
+	data: () => ({
+		showPassword: false,
+		links: ['Home', 'Login'],
+	}),
+}
+</script>
